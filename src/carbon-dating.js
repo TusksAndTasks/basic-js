18050e271d3db7675798463b3f49f24d2883fe75
TusksAndTasks <thedestroyerofworlds@mail.ru>
2021-10-05 02:53:52 +0300

    feat: half of carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,23 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+   let k = 0.693 / HALF_LIFE_PERIOD;
+
+
+   if (typeof sampleActivity == String && sampleActivity > 0){
+      let logOfNoDividedOnn = Math.log(MODERN_ACTIVITY/sampleActivity);
+      let t = logOfNoDividedOnn / k;
+      return Math.ceil(t);
+      } 
+
+   else if (sampleActivity <= 0){
+     return false;
+   }
+    
+   else {
+     return false;
+   }
+
 }
+

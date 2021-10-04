79b2dc4455cb78ad048a8a75989580abd2a2f601
TusksAndTasks <thedestroyerofworlds@mail.ru>
2021-10-05 02:15:00 +0300

    feat: half of what season

--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,27 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason( date ) {
+
+  if (date === undefined) {
+     return 'Unable to determine the time of year!';
+  }
+
+  let month = date.getMonth();
+
+  if (month === 0 || month === 1 || month === 11){
+    return 'winter';
+  }
+
+  else if (month === 2 || month === 3 || month === 4){
+    return 'spring';
+  }
+
+  else if (month === 5 || month === 6 || month === 7){
+    return 'summer';
+  }
+
+  else if (month === 8 || month === 9 || month === 10){
+    return 'autumn';
+  }
 }
